ef5e07bc6cfc1eae6f44f6a57f8e62aff787a100
yehezavi <35952478+yehezavi@users.noreply.github.com>
2018-02-15 11:34:15 +0200

    Compare algorithms

--- AngryBirdsBGU/src/MetaAgent/Main.java
+++ AngryBirdsBGU/src/MetaAgent/Main.java
@@ -1,9 +1,20 @@
 package MetaAgent;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
 import AlgorithmTester.AlgorithmTesterDynamicProgramming;
+import AlgorithmTester.AlgorithmTesterDynamicProgrammingBinned;
+import AlgorithmTester.AlgorithmTesterGreedy;
+import AlgorithmTester.AlgorithmTesterGreedy2;
+import AlgorithmTester.AlgorithmTesterRandom;
 import DB.DBHandler;
 import DB.Data;
 import DB.Queries;
+import DB.ValueExtractor.ValueExtractorScore;
+import DB.ValueExtractor.ValueExtractorTimeTaken;
 
 public class Main {
 
@@ -13,15 +24,55 @@ public class Main {
 			
 //			new MetaAgentDistributionSampling(600, new String[] {"planA", "naive", "AngryBER", "ihsev"}).start();
 			
-			new AlgorithmTesterDynamicProgramming(600).test(100);
+
+			
+//
 			
 			
 //			Data data = DBHandler.loadData();
-//			new Queries().getLevelResults(data );
+//			new Queries().getLevelsResults(data );
+//			System.out.println("score:");
+//			new Queries().getSupportSize(data, new ValueExtractorScore());
+//			System.out.println("time:");
+//			new Queries().getSupportSize(data, new ValueExtractorTimeTaken());
 			
+//			new Queries().getAgentLevelWinsCount(data);
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	static void compareAlgorithms() throws Exception {
+		File folder = new File(Constants.levelsDir);
+		File[] listOfFiles = folder.listFiles();
+		
+		String[] levelsBank = new String[20];
+		for (int i=0; i<20; i++) {
+			levelsBank[i] = listOfFiles[i*20].toPath().getFileName().toString().replace(".json", "");;
+		}
+		
+		while(true) {
+			Problem problem = new Problem();
+			problem.agents = new ArrayList<>(Arrays.asList("planA,naive,AngryBER,ihsev".split(",")));
+			problem.timeConstraint = 300;
+			
+			HashSet<String> levels = new HashSet<>();
+			while(levels.size() < 3) {
+				levels.add(levelsBank[(int) (Math.random() * levelsBank.length)]);
+			}
+			
+			problem.levels =new ArrayList<>(levels);  //new ArrayList<>(Arrays.asList("Level8-1,Levelcherryblossom-4,Level159,Level5-2".split(",")));
+			String[] additionalData = {null};
+			new AlgorithmTesterDynamicProgramming(problem).test(100, additionalData);
+			new AlgorithmTesterDynamicProgrammingBinned(problem, 1, 10).test(100, additionalData);
+			new AlgorithmTesterDynamicProgrammingBinned(problem, 1, 25).test(100, additionalData);
+			new AlgorithmTesterDynamicProgrammingBinned(problem, 1000, 1).test(100, additionalData);
+			new AlgorithmTesterDynamicProgrammingBinned(problem, 1000, 10).test(100, additionalData);
+			new AlgorithmTesterGreedy(problem).test(100, additionalData);
+			new AlgorithmTesterGreedy2(problem).test(100, additionalData);
+			new AlgorithmTesterRandom(problem).test(100, additionalData);
+//			System.out.println("levels: " + String.join(",", problem.levels) + ". average score = " + score + ". " + additionalData);
+		}		
 	}
 }
